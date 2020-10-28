9f2f47a140949044a495906397869ec1b9df5dfd
rust7 <rust.91@icloud.com>
2020-10-28 20:26:01 +0100

    Register/SignUp UI

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,21 +1,24 @@
 import React from 'react'
 import { Formik, Form,  } from 'formik'
-import { FormControl, FormLabel, Input, FormErrorMessage } from '@chakra-ui/core';
+import { Box, Button } from '@chakra-ui/core';
 import { Wrapper } from '../components/Wrapper';
+import { InputField } from '../components/InputField';
 
 interface registerProps {}
 
 const Register: React.FC<registerProps> = ({}) => {
     return (
         <Wrapper variant='small'>
-            <Formik initialValues={{ username: '', password: '' }} onSubmit={values => { console.log(values);}}>
-                {({values, handleChange}) => (
+            <Formik initialValues={{ username: '', password: '' }} onSubmit={values => { console.log(values) ;}}>
+                {({ isSubmitting }) => (
                     <Form>
-                        <FormControl>
-                            <FormLabel htmlFor="username">Username</FormLabel>
-                            <Input value={values.username} onChange={handleChange} id="username" placeholder="Username" />
-                            {/* <FormErrorMessage></FormErrorMessage> */}
-                        </FormControl>
+                        <InputField name='username' placeholder='Enter username' label='Username'></InputField>
+                        
+                        <Box mt={4}>
+                            <InputField name='password' placeholder="Enter password" label='Password' type="password"></InputField>
+                        </Box>
+
+                        <Button type="submit" isLoading={isSubmitting} mt={4} size="md" variantColor="green" variant="solid">Register</Button>
                     </Form>
                 )}
             </Formik>
