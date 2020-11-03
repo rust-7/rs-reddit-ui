13046dff466b5dc9aeb7e7bc427d5e5f2964c6ef
rust7 <rust.91@icloud.com>
2020-11-03 20:36:13 +0100

    Mock Data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,37 +3,28 @@ import { withUrqlClient } from "next-urql";
 import { createUrqlClient } from "../utils/createUrqlClient";
 import { usePostsQuery } from "../generated/graphql";
 import { Layout } from "../components/Layout";
-import { Button, Icon, Link } from "@chakra-ui/core";
+import { Link } from "@chakra-ui/core";
 import NextLink from "next/link";
-import React from "react";
 
 const Index = () => {
-	const [{ data }] = usePostsQuery({
-		variables: {
-			limit: 10,
-		}
-	});
-	return (
-		<Layout>
-
-			<NextLink href="/create-post">
-				<Link color="blue.500">
-					<Button variantColor="green">
-						Create <Icon name="external-link" mx="2px" />
-					</Button>
-				</Link>
-			</NextLink>
-
-			<h1>RUST_REDDIT</h1>
-
-			<br />
-
-			{ !data ?
-				(<div>Loading...</div>) :
-				(data.posts.map((p) => <div key={p.id}>{p.title}</div>))
-			}
-		</Layout>
-	);
+  const [{ data }] = usePostsQuery({
+    variables: {
+      limit: 10,
+    },
+  });
+  return (
+    <Layout>
+      <NextLink href="/create-post">
+        <Link>create post</Link>
+      </NextLink>
+      <br />
+      {!data ? (
+        <div>loading...</div>
+      ) : (
+        data.posts.map((p) => <div key={p.id}>{p.title}</div>)
+      )}
+    </Layout>
+  );
 };
 
 export default withUrqlClient(createUrqlClient, { ssr: true })(Index);
